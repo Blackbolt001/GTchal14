4ed278e7ed5bf597f7175b79ef6390088f07c621
unknown <Jgossett73@gmail.com>
2022-08-23 19:46:47 -0400

    user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,61 +1,53 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const sequelize = require('..config/connection');
+const { User,Post,Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['id','title','content','created_at'],
+    order: [['created_at',"DESC"]],
+    include:[
+{ model:User,
+  attributes:['username'],
+},
+{
+  model:Comment,
+  attributes:[ 'id','comment_text','post_id','user_id','created_at'],
+  include: { model:User,
+  attrubutes:['username'],
+},
+},
+],
+})
+.then((dbPostData) => res.json(dbPostData)) 
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
 });
-
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
-
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
+  
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: { id:req.params.id,
+},
+attributes:['id','content','title','created_at'],
+include: [{ model:User,
+attributes:['username'],
+},
+{model:Comment,
+  attributes:['id','comment_text', 'post_id','user_id','created_at'],
+  include:{
+    model: User,
+    attributes: ['username'],
+  },
+},],
+})
+.then((dbPostData) => {
+  if (!dbPostData) {
+    res.status(404).json({message:'No such Posts exists!'});
+    return;
   }
-});
-
+  res.json(dbPostData;)
+})
 module.exports = router;
