96c8d377ff752ae55873db7f5737f0d3755d558f
unknown <Jgossett73@gmail.com>
2022-08-23 19:17:55 -0400

    controlers API routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,37 +1,28 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { User,Post,Comment} = require('../../models');
+const withAuth = require('../utils/auth');
 
-router.post('/', async (req, res) => {
-  try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
+router.get('/', (req, res) => {
+Comment.findAll(
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
     });
-
-    res.status(200).json(newProject);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+});
+router.post('/', withAuth, (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id:req.session.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
-    }
-
-    res.status(200).json(projectData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,37 +1,53 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const sequelize = require('..config/connection');
+const { User,Post,Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.post('/', async (req, res) => {
-  try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newProject);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
-    }
-
-    res.status(200).json(projectData);
-  } catch (err) {
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['id','title','content','created_at'],
+    order: [['created_at',"DESC"]],
+    include:[
+{ model:User,
+  attributes:['username'],
+},
+{
+  model:Comment,
+  attributes:[ 'id','comment_text','post_id','user_id','created_at'],
+  include: { model:User,
+  attrubutes:['username'],
+},
+},
+],
+})
+.then((dbPostData) => res.json(dbPostData)) 
+  .catch(err => {
+    console.log(err);
     res.status(500).json(err);
-  }
 });
-
+});
+  
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: { id:req.params.id,
+},
+attributes:['id','content','title','created_at'],
+include: [{ model:User,
+attributes:['username'],
+},
+{model:Comment,
+  attributes:['id','comment_text', 'post_id','user_id','created_at'],
+  include:{
+    model: User,
+    attributes: ['username'],
+  },
+},],
+})
+.then((dbPostData) => {
+  if (!dbPostData) {
+    res.status(404).json({message:'No such Posts exists!'});
+    return;
+  }
+  res.json(dbPostData;)
+})
 module.exports = router;
