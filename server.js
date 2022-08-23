e7fa16dc7a75ecca43c6cadf70eb035031ca3334
unknown <Jgossett73@gmail.com>
2022-08-23 14:19:05 -0400

    server.js set

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const session = require('express-session');
 const routes = require('./controllers');
-
+const path = require('path');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+  helpers
+});
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -18,13 +23,14 @@ const sess = {
   })
 };
 
+app.engine('handlebars',hbs.engine);
+app.set('view engine','handlebars');
 app.use(session(sess));
-
+app.use(express.static(path.join(__dirname,`public`)));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
