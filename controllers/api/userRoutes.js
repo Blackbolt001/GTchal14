60af9c2fec14e6ebb2dabe94937d89c5fe3416ad
unknown <Jgossett73@gmail.com>
2022-08-23 20:08:24 -0400

    added utils and helpers

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,53 +1,94 @@
 const router = require('express').Router();
-const sequelize = require('..config/connection');
 const { User,Post,Comment } = require('../models');
-const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: ['id','title','content','created_at'],
-    order: [['created_at',"DESC"]],
-    include:[
-{ model:User,
-  attributes:['username'],
-},
-{
-  model:Comment,
-  attributes:[ 'id','comment_text','post_id','user_id','created_at'],
-  include: { model:User,
-  attrubutes:['username'],
-},
-},
-],
-})
-.then((dbPostData) => res.json(dbPostData)) 
+  router.get('/', (req, res) => {
+  User.findAll({
+  attributes: {exclude:['password']}
+})   
+  .then((dbUserData) => res.json(dbUserData)) 
   .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+  console.log(err);
+  res.status(500).json(err);
 });
 });
   
-router.get('/:id', (req, res) => {
-  Post.findOne({
-    where: { id:req.params.id,
-},
-attributes:['id','content','title','created_at'],
-include: [{ model:User,
-attributes:['username'],
-},
-{model:Comment,
-  attributes:['id','comment_text', 'post_id','user_id','created_at'],
-  include:{
-    model: User,
-    attributes: ['username'],
-  },
-},],
+  router.get('/:id', (req, res) => {  
+  User.findOne({
+  attributes: {exclude:['password']},
+  where: {id:req.params.id},
+  include:[{model:Post,
+  attributes:['id','content','title','created_at']},
+  {model:Comment,
+  attributes:['id','comment_text','created_at'],
+  include: [{ model:Post,
+  attributes:['title']
+}]
+}]
 })
-.then((dbPostData) => {
-  if (!dbPostData) {
-    res.status(404).json({message:'No such Posts exists!'});
-    return;
-  }
-  res.json(dbPostData;)
+  .then(dbUserData => {
+  if(!dbUserData) {
+  res.status(404).json({message:'User does not exist!'});
+  return;
+}
+  res.json(dbUserData);
 })
+});
+  router.post('/', (req,res) => {
+  User.create({
+  username:req.body.username,
+  password:req.body.password
+  })
+  .then(dbUserData => {
+    req.session.save(() => {
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
+    res.json(dbUserData);
+  });
+})
+.catch(err => {
+console.log(err);
+res.status(500).json(err);
+});
+})
+  router.post('/login', (req,res) => {
+  User.findOne({
+    where: {username:req.body.username}
+})
+  .then(dbUserData => {
+  if (!dbUserData) {
+  res.status(400).json({message:'User does not exist!'});
+  return;
+}
+    req.session.save(() => {
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
+    res.json({dbUserData, message:'Welcome to the Party!!'}); 
+  });
+});
+const validPassword = dbUserData.checkPassword(req.body.password);
+if(!validPassword) {
+  res.status(400).json({message:'Please try again'});
+  return;
+}
+req.session.save(() => {
+  req.session.user_id = dbUserData.id;
+  req.session.username = dbUserData.username;
+  req.session.loggedIn = true;
+  res.json({ user:dbUserData,message:'Welcome to Flavortown!!'});
+  });
+});
+router.post('/logout', (req,res) => {
+  if(req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+    }else {
+      res.status(404).end();
+    }
+  });
+
+
+  
 module.exports = router;
